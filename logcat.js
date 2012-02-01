991cc90e7d4ae51f998ccec5dd4975312008b358
Michael DiGiovanni <vinblau@gmail.com>
2012-02-01 05:34:13 -0500

    Cleaned up formatting and useless code, comments.

--- logcat.js
+++ logcat.js
@@ -1,125 +1,97 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-//  , util  = require('util')
-  , url = require('url')
-  , spawn = require('child_process').spawn
-  , sutil = require('./sutil');
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    url = require('url'),
+    spawn = require('child_process').spawn,
+    sutil = require('./sutil');
 
 app.listen(1337);
 
-var buffer = '';
-var sockets = [];
-var history = [];
+var buffer = ''; //Left overs for when we read from stdout/logcat
+var sockets = []; //All socket.io connections
+var history = []; //All logcat lines, used to send recent history to a new client
 
-function handler (req, res) {
-  var urlHandler = sutil.getMap[url.parse(req.url).pathname] || sutil.not_found; //FIXME: put generic here
-  res.simpleJSON = function(code, obj) {
-    var body = JSON.stringify(obj);
-    res.writeHead(code, {
-    	'Content-Type': 'text/json',
-    	'Content-Length': body.length
-    });
-    res.write(body);
-    res.end();
-  };
-  urlHandler(req, res);
-/*  
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-*/
+function handler(req, res) {
+    var urlHandler = sutil.getMap[url.parse(req.url).pathname] || sutil.not_found; //FIXME: put generic here
+    res.simpleJSON = function (code, obj) {
+        var body = JSON.stringify(obj);
+        res.writeHead(code, {
+            'Content-Type': 'text/json',
+            'Content-Length': body.length
+        });
+        res.write(body);
+        res.end();
+    };
+    urlHandler(req, res);
 }
 
 function sendMessage(data) {
-  //FUCK regex
-  //maybe split by spaces up to the 4th space?
-  var logLine = {};
-  var splits = data.split(/ {1,99}/, 6); //only break between 1 and 99 space sin a row
+    var logLine = {};
+    var splits = data.split(/ {1,99}/, 6); //only break between 1 and 99 space sin a row
     //FIXME: need to switch to logcat long to get everything safely
-  logLine["date"] = splits[0];
-  logLine["time"] = splits[1];
-  logLine["process"] = splits[2]
-  //Skip one, that isn't process
-  logLine["priority"] = splits[4];
-  logLine["tag"] = splits[5];
-  //String.replace(pattern,string)
-  //so data.replace(   , ''); //replace first 5 peices?
-  //.split(/ {1,99} {1,99} {1,99} {1,99} {1,99}/, 7);
+    logLine["date"] = splits[0];
+    logLine["time"] = splits[1];
+    logLine["process"] = splits[2]
+    //Skip one, that isn't process
+    logLine["priority"] = splits[4];
+    logLine["tag"] = splits[5];
+    logLine["message"] = data.replace(/[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}/, '');
 
-  //"01-24 09:57:20.077 16223 16241 I AmazonAppstore.ApplicationLockerImpl: PERFORMANCE done loading locker".replace(  /\.*s/,'');
-  //FIXME: add this back
-  logLine["message"] = data.replace(/[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}[^ ]* {1,99}/, '');
+    //For debugging, shove the entire line in
+    logLine["fullLine"] = data;
 
-  //For debugging, shove the entire line in
-  logLine["fullLine"] = data;
-  history.push(logLine);
-  if (history.length > 100) {
-    //FIXME: cut off pieces
-    //Definately need to add this or it chokes the browser.. yikes
-  }
-  //Now send it to every connected socket
-  sockets.forEach(function(socket) {
-    socket.emit('logcat', { buffer: logLine });
-  });
+    //Now add this line to history so we can send it to new clients
+    history.push(logLine);
+    if (history.length > 100) {
+        //FIXME: cut off pieces
+        //Definately need to add this or it chokes the browser.. yikes
+    }
+    //Now send it to every connected socket
+    sockets.forEach(function (socket) {
+        socket.emit('logcat', {
+            buffer: logLine
+        });
+    });
 }
-
 
 //Track sockets
 io.sockets.on('connection', function (socket) {
-  sockets.push(socket);
+    sockets.push(socket);
 
-    //FIXME: figure out ordering, might wind up adding items more than once?
-    //history.forEach(function(logLine) {
-    //    socket.emit('logcat', { buffer: logLine });
     //Looping through the history is a bad idea, lets just batch the entire thing up
-        socket.emit('logcatHistory', { buffer: history });
+    socket.emit('logcatHistory', {
+        buffer: history
+    });
     //});
-  //mayeb end or close, dahl mentioned those in intro
-  socket.on('disconnect', function () {
-    console.log('Disconnected');
-    var i = sockets.indexOf(socket);
-    delete sockets[i];
-  });
+    //mayeb end or close, ryan dahl mentioned those in intro
+    //Not sure difference between end, close, disconnect
+    socket.on('disconnect', function () {
+        console.log('Disconnected');
+        var i = sockets.indexOf(socket);
+        delete sockets[i];
+    });
 });
 
 
 function createLogcatProcess() {
-  var logcat = spawn('adb', ['logcat', '-v', 'threadtime']);
-  logcat.stdout.setEncoding('utf8'); //Check that this does anything
-  logcat.stdout.on('data', function (data) {
-    //How do they get application name????
-    //From logcat -v threadtime
-    //01-24 09:57:20.077 16223 16241 I AmazonAppstore.ApplicationLockerImpl: PERFORMANCE done loading locker
-    //From eclipse
-    //01-24 09:48:07.428: D/NotificationSyndication(23097): onAccessibilityEvent
-    buffer = buffer + data; //Append...
-    var lines = data.split('\n');
-    //for(var x = 0; x < lines.length; x++) {
-//    if (lines[lines.size - 1] != '') {
-      //I think this doesn't matter, we either add an empty or we add incomplete and remove from processing
-      buffer = lines.pop(); //removes last and adds to buffer
-//    } else {
-//      lines.pop(); //remove last cause its empty
-//      buffer = ''; //clear buffer
-//    }
-    lines.forEach(function(line) {
-      sendMessage(line);
-      console.log('************LINE');
-      //FIXME: Need ot check if ends with '/n' right and add to buffer
-    });
+    var logcat = spawn('adb', ['logcat', '-v', 'threadtime']);
+    logcat.stdout.setEncoding('utf8'); //Check that this does anything
+    logcat.stdout.on('data', function (data) {
+        //How does eclipse get application name???? ps grep pid?
+        //From logcat -v threadtime
+        //01-24 09:57:20.077 16223 16241 I AmazonAppstore.ApplicationLockerImpl: PERFORMANCE done loading locker
+        //From eclipse
+        //01-24 09:48:07.428: D/NotificationSyndication(23097): onAccessibilityEvent
+        buffer = buffer + data; //Append...
+        var lines = data.split('\n');
+        buffer = lines.pop(); //removes last and adds to buffer
+        lines.forEach(function (line) {
+            sendMessage(line);
+            //console.log('************LINE');
+        });
 
-    //FIXME: put rest of buffer back in
-    console.log('*********');
-    console.log(data);
-  });
+        //console.log('*********');
+        //console.log(data);
+    });
 }
 
 createLogcatProcess();
--- sutil.js
+++ sutil.js
@@ -1,85 +1,68 @@
+/*
+ * Borrowed and mofidied form a Node chat server, probably
+ * completely different now
+ */
 var sys = require('sys'),
-fs = require('fs'),
-qs = require('querystring'),
-url = require('url'),
-sutil = exports;
+    fs = require('fs'),
+    qs = require('querystring'),
+    url = require('url'),
+    sutil = exports;
 
 sutil.getMap = [];
 
 sutil.sessions = {};
 
-sutil.createSession = function(nick) {
-	var i, session;
-
-	for (i in sutil.sessions) {
-		session = sutil.sessions[i];
-		if (session && session.nick === nick) {
-			return null;
-		}
-	}
-
-	session = {
-		nick: nick,
-		id: Math.floor(Math.random() * 999999999999).toString()
-	};
-
-	sutil.sessions[session.id] = session;
-	return session;
+sutil.get = function (path, handler) {
+    sutil.getMap[path] = handler;
 };
 
-sutil.get = function(path, handler) {
-	sutil.getMap[path] = handler;
+sutil.not_found = function (req, res) {
+    var not_found_msg = 'Not Found';
+    console.log('Not found: ' + req.url);
+    res.writeHead(404, {
+        'Content-Type': 'text/plain',
+        'Content-Length': not_found_msg.length
+    });
+    res.write(not_found_msg);
+    res.end();
 };
 
-sutil.not_found = function(req, res) {
-	var not_found_msg = 'Not Found';
-     console.log('Not found: ' + req.url);
-	res.writeHead(404, {
-		'Content-Type': 'text/plain',
-		'Content-Length': not_found_msg.length
-	});
-	res.write(not_found_msg);
-	res.end();
-};
+sutil.staticHandler = function (filename) {
+    var body;
 
-sutil.staticHandler = function(filename) {
-	var body;
+    function loadResponseData(callback) {
+        fs.readFile(filename, function (err, data) {
+            if (err) {
+                sys.debug('Error loading file ' + filename);
+            } else {
+                sys.debug('loading file ' + filename);
+                body = data;
+            }
+            callback();
+        });
+    }
 
-	function loadResponseData(callback) {
-		fs.readFile(filename, function(err, data) {
-			if (err) {
-				sys.debug('Error loading file ' + filename);
-			} else {
-				sys.debug('loading file ' + filename);
-				body = data;
-			}
-			callback();
-		});
-	}
-
-	return function(req, res) {
-		loadResponseData(function() {
-			res.writeHead(200, {
-				'Content-Type': 'text/html',
-				'Content-Length': body.length
-			});
-			res.write(body);
-			res.end();
-		});
-	};
+    return function (req, res) {
+        loadResponseData(function () {
+            res.writeHead(200, {
+                //FIXME: Pull these from a map based on extension
+                'Content-Type': 'text/html',
+                'Content-Length': body.length
+            });
+            res.write(body);
+            res.end();
+        });
+    };
 
 };
 
+//FIXME: make this generic
 sutil.get('/', sutil.staticHandler('index.html'));
 sutil.get('/jquery.dataTables.min.js', sutil.staticHandler('jquery.dataTables.min.js'));
 sutil.get('/jquery.js', sutil.staticHandler('jquery.js'));
 sutil.get('/style.css', sutil.staticHandler('style.css'));
 sutil.get('/tables.css', sutil.staticHandler('tables.css'));
 sutil.get('/jquery-ui-1.8.17.custom.css', sutil.staticHandler('trontastic/jquery-ui-1.8.17.custom.css'));
-sutil.get('/images/ui-bg_gloss-wave_85_9fda58_500x100.png', sutil.staticHandler('trontastic/images/ui-bg_gloss-wave_85_9fda58_500x100.png'));
-sutil.get('/images/ui-icons_b8ec79_256x240.png', sutil.staticHandler('trontastic/images/ui-icons_b8ec79_256x240.png'));
-sutil.get('/images/ui-bg_glass_40_0a0a0a_1x400.png', sutil.staticHandler('trontastic/images/ui-bg_glass_40_0a0a0a_1x400.png'));
-
 sutil.get('/images/forward_disabled.jpg', sutil.staticHandler('images/forward_disabled.jpg'));
 sutil.get('/images/back_disabled.jpg', sutil.staticHandler('images/back_disabled.jpg'));
 sutil.get('/images/forward_enabled.jpg', sutil.staticHandler('images/forward_enabled.jpg'));
@@ -88,34 +71,16 @@ sutil.get('/images/sort_desc.png', sutil.staticHandler('images/sort_desc.png'));
 sutil.get('/images/sort_asc.png', sutil.staticHandler('images/sort_asc.png'));
 sutil.get('/images/sort_both.png', sutil.staticHandler('images/sort_both.png'));
 
-sutil.get('/join', function(req, res) {
-	var nick = qs.parse(url.parse(req.url).query).nick,
-	session;
+//FIXME: just left in as a simple sample, it's dumb
+sutil.get('/who', function (req, res) {
+    var nicks = [],
+        i, session;
 
-	session = sutil.createSession(nick);
-	if (!session) {
-		res.simpleJSON(200, {
-			error: 'Nick in use'
-		});
-		return;
-	}
-
-	res.simpleJSON(200, {
-		nick: session.nick,
-		id: session.id
-	});
-});
-
-sutil.get('/who', function(req, res) {
-	var nicks = [],
-	i,
-	session;
-
-	for (i in sutil.sessions) {
-		session = sutil.sessions[i];
-		nicks.push(session.nick);
-	}
-	res.simpleJSON(200, {
-		nicks: nicks
-	});
+    for (i in sutil.sessions) {
+        session = sutil.sessions[i];
+        nicks.push(session.nick);
+    }
+    res.simpleJSON(200, {
+        nicks: nicks
+    });
 });
